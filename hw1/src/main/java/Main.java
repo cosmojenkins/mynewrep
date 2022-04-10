776aa8c4482287513abe65fc1301cf532daace45
Fedor_Bocharov <cosmo.jenkins@rambler.ru>
2022-04-11 00:22:02 +0300

    hw1

--- hw1/src/main/java/Main.java
+++ hw1/src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.print("Hello ");
+        mWorld();
+    }
+
+    public static void mWorld() {
+        System.out.println("World!");
     }
 }
